a96714dc2508247f3617e117d82da24198b9ca8c
이승훈 <leesh6172@viking-lab.com>
2024-03-05 13:59:37 +0900

    Feat: thisDay 컴포넌트 API 날씨 데이터 가져오기 Feat: openweather API 연결 Comment : API 주석 추가, Rename: gitignore .env 파일(API)키 제외 추가 Remove: reportWebVitals 기능 제거 및 파일 삭제

--- src/components/ThisDay/index.tsx
+++ src/components/ThisDay/index.tsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import useWeather from '../../utils/useWeather'
+import { useParams } from 'react-router-dom'
 
 const ThisDay = () => {
+
+  const { id } = useParams();
+  const { data, isLoading } = useWeather(id ? id : 'seoul');
+
+  // 확인용
+  console.log(data);
+  console.log(isLoading);
+
+  const temperature = Math.round(data?.main.temp || 0);
+  const cityName = data?.name;
+
+  // weather 날씨 아이콘
+  const weatherIcon = data?.weather[0].main;
+  const weatherId = data?.weather[0].id;
+  let imageSrc = "./images/mainly_cloudy.svg";
+  if (weatherIcon === "Clear") {
+    imageSrc = "./images/weatherIcons/clear-sky.svg";
+  } else if (weatherIcon === "Clouds" && (weatherId || 0) >= 803) {
+    imageSrc = "./images/weatherIcons/broken-clouds.svg";
+  } else if (weatherIcon === "Clouds" && (weatherId || 0) < 803) {
+    imageSrc = "./images/weatherIcons/few-clouds.svg";
+  } else if (weatherIcon === "Atmosphere") {
+    imageSrc = "./images/weatherIcons/mist.svg";
+  } else if (weatherIcon === "Rain") {
+    imageSrc = "./images/weatherIcons/rain.svg";
+  } else if (weatherIcon === "Rain" && (weatherId || 0) > 505) {
+    imageSrc = "./images/weatherIcons/shower-rain.svg";
+  } else if (weatherIcon === "Snow") {
+    imageSrc = "./images/weatherIcons/snow.svg";
+  } else if (weatherIcon === "Thunderstorm") {
+    imageSrc = "./images/weatherIcons/thunderstorm.svg";
+  }
+  // 테스트용
+  console.log(weatherIcon);
+
+
   return (
-    <div>ThisDay</div>
+    <div>
+
+
+    </div>
   )
 }
 
---
+++ src/utils/openWeather.ts
@@ -0,0 +1,11 @@
+import OpenWeatherMap from "openweathermap-ts";
+
+// openweather API 활용
+const openWeather = new OpenWeatherMap({
+    // openweathermap 홈페이지 api 발급 참조
+    apiKey: process.env.REACT_APP_API_KEY as string
+})
+
+openWeather.setUnits('metric');
+
+export default openWeather;

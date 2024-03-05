a3f151de3dcf05f6e9109428661a135574668605
이승훈 <leesh6172@viking-lab.com>
2024-03-05 15:54:42 +0900

    Feat: ThisDayInfo 기능 추가 Design : ThisDayInfo 스타일 추가

--- src/components/ThisDayInfo/index.tsx
+++ src/components/ThisDayInfo/index.tsx
@@ -1,8 +1,53 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
+import useWeather from '../../utils/useWeather';
+import { ImgWrapper, ThisDayInfoWrapper } from './styles';
+
+
+const nameInfo = ["온도", "기압", "습도", "바람"];
+const imgInfo = [
+  "./images/temperature.svg",
+  "./images/pressure.svg",
+  "./images/humidity.svg",
+  "./images/wind.svg",
+];
 
 const ThisDayInfo = () => {
+
+  const { id } = useParams();
+  const { data, isLoading } = useWeather(id ? `${id}` : "seoul");
+
+  const temp = `${Math.round(data?.main.temp || 0)}°C feels like ${Math.round(
+    data?.main.feels_like || 0
+  )}°C`;
+  const pressureMetric = Math.round((data?.main.pressure || 0) / 1.333);
+  const pressure = `${pressureMetric} mmHg  - ${pressureMetric < 748 ? "low" : pressureMetric > 770 ? "high" : "normal"
+    }`;
+  const humidity = `${data?.main.humidity} %`;
+  const wind = `${Math.round((data?.wind.speed || 0) * 10)} mph`;
+  const dataInfo = [temp, pressure, humidity, wind];
+
   return (
-    <div>ThisDayInfo</div>
+    <ThisDayInfoWrapper>
+      <div>
+        {imgInfo.map((img) => (
+          <ImgWrapper>
+            <img src={img} alt="" />
+          </ImgWrapper>
+        ))}
+      </div>
+      <div>
+        {nameInfo.map((inf) => (
+          <h2>{inf}</h2>
+        ))}
+      </div>
+
+      <div>
+        {dataInfo.map((info) => (
+          <p>{info}</p>
+        ))}
+      </div>
+    </ThisDayInfoWrapper>
   )
 }
 
